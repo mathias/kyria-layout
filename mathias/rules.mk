e386ef426ec6fe274b85ff134344e2b9aaf1a6fa
Matt Gauger <matt.gauger@gmail.com>
2021-09-06 13:30:48 -0500

    Another small clean up

--- mathias/rules.mk
+++ mathias/rules.mk
@@ -1,7 +1,6 @@
 OLED_ENABLE = no
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-#RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 
 LEADER_ENABLE = no         # Enable the leader key feature. See readme.md.
 
