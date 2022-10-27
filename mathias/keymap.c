55ff4de3c9275914a84b4ec2a5cb29c44bb31cfc
Matt Gauger <matt.gauger@gmail.com>
2022-10-27 16:57:40 -0500

    Put a backtick where I had tilde; also broke and fixed QMK somehow

--- mathias/keymap.c
+++ mathias/keymap.c
@@ -25,66 +25,66 @@ enum layers {
     _SYM2,
 };
 
-#ifdef OLED_ENABLE
-bool oled_task_user(void) {
-  if (is_keyboard_master()) {
-    // QMK Logo and version information
-    // clang-format off
-    static const char PROGMEM qmk_logo[] = {
-      0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-      0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-      0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-    // clang-format on
-
-    oled_write_P(qmk_logo, false);
-    oled_write_P(PSTR("Kyria - mathias\n\n"), false);
-
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
-    switch (get_highest_layer(layer_state | default_layer_state)) {
-      case _QWERTY:
-        oled_write_P(PSTR("QWERTY\n"), false);
-        break;
-      case _COLEMAK_DH:
-        oled_write_P(PSTR("Colemak-DH\n"), false);
-        break;
-      case _NAV:
-        oled_write_P(PSTR("Nav\n"), false);
-        break;
-      case _SYM:
-        oled_write_P(PSTR("Sym\n"), false);
-        break;
-      case _FUNCTION:
-        oled_write_P(PSTR("Function\n"), false);
-        break;
-      case _ADJUST:
-        oled_write_P(PSTR("Adjust\n"), false);
-        break;
-      default:
-        oled_write_P(PSTR("Undefined\n"), false);
-    }
-
-    // Host Keyboard LED Status
-    led_t led_usb_state = host_keyboard_led_state();
-    oled_write_P(led_usb_state.num_lock ? PSTR("NUMLCK ") : PSTR("       "), false);
-    oled_write_P(led_usb_state.caps_lock ? PSTR("CAPLCK ") : PSTR("       "), false);
-    oled_write_P(led_usb_state.scroll_lock ? PSTR("SCRLCK ") : PSTR("       "), false);
-
-    return false;
-  } else {
-    // clang-format off
-    static const char PROGMEM qmk_logo[] = {
-      0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-      0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-      0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-    // clang-format on
-
-    oled_write_P(qmk_logo, false);
-
-    return false;
-  }
-}
-#endif
+// #ifdef OLED_ENABLE
+// bool oled_task_user(void) {
+//   if (is_keyboard_master()) {
+//     // QMK Logo and version information
+//     // clang-format off
+//     static const char PROGMEM qmk_logo[] = {
+//       0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+//       0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+//       0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
+//     // clang-format on
+//
+//     oled_write_P(qmk_logo, false);
+//     oled_write_P(PSTR("Kyria - mathias\n\n"), false);
+//
+//     // Host Keyboard Layer Status
+//     oled_write_P(PSTR("Layer: "), false);
+//     switch (get_highest_layer(layer_state | default_layer_state)) {
+//       case _QWERTY:
+//         oled_write_P(PSTR("QWERTY\n"), false);
+//         break;
+//       case _COLEMAK_DH:
+//         oled_write_P(PSTR("Colemak-DH\n"), false);
+//         break;
+//       case _NAV:
+//         oled_write_P(PSTR("Nav\n"), false);
+//         break;
+//       case _SYM:
+//         oled_write_P(PSTR("Sym\n"), false);
+//         break;
+//       case _FUNCTION:
+//         oled_write_P(PSTR("Function\n"), false);
+//         break;
+//       case _ADJUST:
+//         oled_write_P(PSTR("Adjust\n"), false);
+//         break;
+//       default:
+//         oled_write_P(PSTR("Undefined\n"), false);
+//     }
+//
+//     // Host Keyboard LED Status
+//     led_t led_usb_state = host_keyboard_led_state();
+//     oled_write_P(led_usb_state.num_lock ? PSTR("NUMLCK ") : PSTR("       "), false);
+//     oled_write_P(led_usb_state.caps_lock ? PSTR("CAPLCK ") : PSTR("       "), false);
+//     oled_write_P(led_usb_state.scroll_lock ? PSTR("SCRLCK ") : PSTR("       "), false);
+//
+//     return false;
+//   } else {
+//     // clang-format off
+//     static const char PROGMEM qmk_logo[] = {
+//       0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+//       0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+//       0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
+//     // clang-format on
+//
+//     oled_write_P(qmk_logo, false);
+//
+//     return false;
+//   }
+// }
+// #endif
 
 
 // Aliases for readability
@@ -179,7 +179,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
    * |        | Tab  |  $   |  '"  |   (  |  )   |                              | - _  |  4   |  5   |  6   |  \|  |        |
    * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
-   * |        |  %   |  ^   |  ~   |   [  |  ]   |      |      |  |      |      |  0   |  1   |  2   |  3   |  =+  |        |
+   * |        |  %   |  ^   |  `~  |   [  |  ]   |      |      |  |      |      |  0   |  1   |  2   |  3   |  =+  |        |
    * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
    *                        |      |      |LSHFT |      |      |  |      |      |      |      |      |
    *                        |      |      |      |      |      |  |      |      |      |      |      |
@@ -188,7 +188,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        [_SYM] = LAYOUT(
          _______, KC_EXCLAIM, KC_AT, KC_PIPE, KC_LCBR, KC_RCBR,                                     KC_UNDERSCORE, KC_7, KC_8, KC_9, KC_KP_ASTERISK, _______,
          _______, KC_TAB, KC_DOLLAR, KC_QUOT, KC_LPRN, KC_RPRN,                                    KC_MINS, KC_4, KC_5, KC_6, KC_BSLS, _______,
-         _______, KC_PERC, KC_CIRC, KC_TILD, KC_LBRC, KC_RBRC, _______, _______, _______, _______, KC_0, KC_1, KC_2, KC_3, KC_EQUAL, _______,
+         _______, KC_PERC, KC_CIRC, KC_GRAVE, KC_LBRC, KC_RBRC, _______, _______, _______, _______, KC_0, KC_1, KC_2, KC_3, KC_EQUAL, _______,
                                     _______, _______, KC_LSFT, _______, _______, _______, _______, _______, _______, _______
        ),
 
